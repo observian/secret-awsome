986119f0db8369e741f607cdf4e18d95807531dc
Jaaromy Zierse <jaaromy.zierse@observian.com>
2018-04-03 17:12:26 -0600

    Get all parameters test.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const {
 	BrowserWindow
 } = require('electron');
 const path = require('path');
-const url = require('url');
+var url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -56,3 +56,10 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+let ssm = require(`${process.cwd()}/src/api/ssm`);
+
+ssm.getAllParameters()
+	.then((params) => {
+		console.log(JSON.stringify(params));
+		console.log('Done');
+	});
