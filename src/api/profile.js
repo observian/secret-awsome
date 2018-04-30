3d09db78470778e05ce33ff72786725e38cf9f1a
Jaaromy Zierse <jaaromy.zierse@observian.com>
2018-04-30 17:28:51 -0600

    Fix bug #2

--- src/api/profile.js
+++ src/api/profile.js
@@ -1,12 +1,20 @@
 const Promise = require('bluebird');
 const fs = require('fs');
+const path = require('path');
+const mkdirp = require('mkdirp');
 Promise.promisifyAll(fs);
 
 const os = require('os');
 const Reader = require('line-by-line');
 
 function getFilePath() {
-	return process.env.AWS_SHARED_CREDENTIALS_FILE ? process.env.AWS_SHARED_CREDENTIALS_FILE : `${os.homedir()}/.aws/credentials`;
+	return process.env.AWS_SHARED_CREDENTIALS_FILE ? process.env.AWS_SHARED_CREDENTIALS_FILE : `${os.homedir()}/.awy/credentials`;
+}
+
+// Create empty credential file if it does not exist
+if (!fs.existsSync(getFilePath())) {
+	mkdirp.sync(path.dirname(getFilePath()), '0750');
+	fs.closeSync(fs.openSync(getFilePath(), 'a'));
 }
 
 function getReader() {
